97abdc4162948fa2c31e14625c5a27e5017c2846
Shola Nur <shola.nur@gmail.com>
2023-02-28 14:52:13 -0800

    more exercies added

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ function fullName(firstName, lastName) {
 }
 console.log(fullName("Shola", "Nurlanova"));
 console.log(fullName("Nurlanova", "Shola"));
+
+let names = ["john", "boob", "josh", "ben", "adilet"];
+
+// length
+console.log(names.length);
+console.log(names[names.length - 1]);
+
+// concat
+
+const lastName = ["pepper", "onion", "alma", "perez"];
+const allNames = names.concat(lastName);
+console.log(allNames);
+console.log(allNames.reverse());
+
+// unshift - adds to beginning of the array
+allNames.unshift("Kate");
+console.log(allNames);
+
+// shift - removing the first item from array
+console.log(allNames.shift());
+console.log(allNames.shift());
+console.log(allNames.shift());
+
+// splice - mutates original array
+const spesNames = allNames.splice(2, 1);
+console.log(spesNames);
